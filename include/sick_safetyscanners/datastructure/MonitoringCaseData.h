797d5e4a2f61f78a83c1e1b0b61f5df4b1027bc8
Lennart Puck <puck@fzi.de>
2018-11-29 14:49:51 +0100

    added monitoring case number and corresponding fields in datastructure

--- include/sick_safetyscanners/datastructure/MonitoringCaseData.h
+++ include/sick_safetyscanners/datastructure/MonitoringCaseData.h
@@ -43,7 +43,7 @@ namespace datastructure {
 
 
 /*!
- * \brief TODO
+ * \brief Stores the data for the different monitoring cases
  */
 class MonitoringCaseData
 {
@@ -65,10 +65,56 @@ public:
    *
    * \param is_valid if the monitoring data is valid.
    */
-  void setIsValid(bool is_valid);
+  void setIsValid(const bool is_valid);
+
+
+  /*!
+   * \brief Returns the number of the monitoring case.
+   *
+   * \returns The number of the monitoring case.
+   */
+  uint16_t getMonitoringCaseNumber() const;
+
+  /*!
+   * \brief Sets the monitoring case number.
+   *
+   * \param monitoring_case_number The monitoring case number.
+   */
+  void setMonitoringCaseNumber(const uint16_t monitoring_case_number);
+
+  /*!
+   * \brief Returns the field indices.
+   *
+   * \returns The field indices.
+   */
+  std::vector<uint16_t> getFieldIndices() const;
+
+  /*!
+   * \brief Sets the field indices.
+   *
+   * \param field_indices The field indices.
+   */
+  void setFieldIndices(const std::vector<uint16_t>& field_indices);
+
+  /*!
+   * \brief Returns if the fields are configured and valid.
+   *
+   * \returns If the fields are valid.
+   */
+  std::vector<bool> getFieldsValid() const;
+
+  /*!
+   * \brief Sets if the fields are valid.
+   *
+   * \param fields_valid if the fields are valid.
+   */
+  void setFieldsValid(const std::vector<bool>& fields_valid);
 
 private:
   bool m_is_valid;
+  uint16_t m_monitoring_case_number;
+  std::vector<uint16_t> m_field_indices;
+  std::vector<bool> m_fields_valid;
 };
 
 
--- src/datastructure/MonitoringCaseData.cpp
+++ src/datastructure/MonitoringCaseData.cpp
@@ -49,6 +49,35 @@ void MonitoringCaseData::setIsValid(bool is_valid)
   m_is_valid = is_valid;
 }
 
+uint16_t MonitoringCaseData::getMonitoringCaseNumber() const
+{
+  return m_monitoring_case_number;
+}
+
+void MonitoringCaseData::setMonitoringCaseNumber(const uint16_t monitoring_case_number)
+{
+  m_monitoring_case_number = monitoring_case_number;
+}
+
+std::vector<uint16_t> MonitoringCaseData::getFieldIndices() const
+{
+  return m_field_indices;
+}
+
+void MonitoringCaseData::setFieldIndices(const std::vector<uint16_t>& field_indices)
+{
+  m_field_indices = field_indices;
+}
+
+std::vector<bool> MonitoringCaseData::getFieldsValid() const
+{
+  return m_fields_valid;
+}
+
+void MonitoringCaseData::setFieldsValid(const std::vector<bool>& fields_valid)
+{
+  m_fields_valid = fields_valid;
+}
 
 } // namespace datastructure
 } // namespace sick
