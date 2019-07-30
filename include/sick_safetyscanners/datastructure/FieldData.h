79b17b66ec6052b2e6e9b1c2bce707771069b4f4
Lennart Puck <puck@fzi.de>
2019-07-30 15:18:12 +0200

    added version to field data

--- include/sick_safetyscanners/datastructure/FieldData.h
+++ include/sick_safetyscanners/datastructure/FieldData.h
@@ -54,6 +54,57 @@ public:
   FieldData();
 
   /*!
+   * \brief Gets the version indicator for the scanner.
+   *
+   * \returns The version indicator for the scanner.
+   */
+  std::string getVersionCVersion() const;
+  /*!
+   * \brief Sets the version indicator for the scanner.
+   *
+   * \param version_c_version The version indicator for the scanner.
+   */
+  void setVersionCVersion(const std::string& version_c_version);
+
+  /*!
+   * \brief Gets the major version number for the scanner.
+   *
+   * \returns The version indicator for the scanner.
+   */
+  uint8_t getVersionMajorVersionNumber() const;
+  /*!
+   * \brief Sets the major version number for the scanner.
+   *
+   * \param version_major_version_number The major version number for the scanner.
+   */
+  void setVersionMajorVersionNumber(const uint8_t& version_major_version_number);
+
+  /*!
+   * \brief Gets the minor version number for the scanner.
+   *
+   * \returns The minor version number for the scanner.
+   */
+  uint8_t getVersionMinorVersionNumber() const;
+  /*!
+   * \brief Sets the minor version number for the scanner.
+   *
+   * \param version_minor_version_number The minor version number for the scanner.
+   */
+  void setVersionMinorVersionNumber(const uint8_t& version_minor_version_number);
+
+  /*!
+   * \brief Gets the version release number for the scanner.
+   *
+   * \returns The version release number for the scanner.
+   */
+  uint8_t getVersionReleaseNumber() const;
+  /*!
+   * \brief Sets the version release number for the scanner.
+   *
+   * \param version_release_number The version release number for the scanner.
+   */
+  void setVersionReleaseNumber(const uint8_t& version_release_number);
+  /*!
    * \brief Returns if the received field data is valid.
    *
    * \returns If the received field data is valid.
