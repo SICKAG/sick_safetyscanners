8227fe411b73212425060667c230ba9e7e8c4a4b
Nico <nico.studt@student.uni-luebeck.de>
2023-06-22 08:14:49 +0200

    Add pending command mutex
    Adding a mutex to restrict simultaneous access to pending commands

--- include/sick_safetyscanners/cola2/Cola2Session.h
+++ include/sick_safetyscanners/cola2/Cola2Session.h
@@ -50,123 +50,119 @@
 #include <limits>
 #include <map>
 
-namespace sick {
-namespace cola2 {
-
-/*!
- * \brief Forward declaration of command class.
- */
-class Command;
-
-
-/*!
- * \brief Forward declaration of create session class.
- */
-class CreateSession;
-
-
-/*!
- * \brief Establishes a cola2 session with a sensor and enables execution of commands in this
- * session.
- */
-class Cola2Session
+namespace sick
 {
-public:
-  /*!
-   * \brief Typedef for a pointer containing a command to be executed.
-   */
-  typedef std::shared_ptr<sick::cola2::Command> CommandPtr;
+  namespace cola2
+  {
 
-  /*!
-   * \brief Constructor of the cola2 session.
-   *
-   * \param async_tcp_client Pointer to an instance of a TCP-client. Will be used to establish a
-   * connection to the sensor.
-   */
-  explicit Cola2Session(const std::shared_ptr<communication::AsyncTCPClient>& async_tcp_client);
+    /*!
+     * \brief Forward declaration of command class.
+     */
+    class Command;
 
+    /*!
+     * \brief Forward declaration of create session class.
+     */
+    class CreateSession;
 
-  /*!
-   * \brief Triggers the disconnection of the tcp socket.
-   */
-  void doDisconnect();
+    /*!
+     * \brief Establishes a cola2 session with a sensor and enables execution of commands in this
+     * session.
+     */
+    class Cola2Session
+    {
+    public:
+      /*!
+       * \brief Typedef for a pointer containing a command to be executed.
+       */
+      typedef std::shared_ptr<sick::cola2::Command> CommandPtr;
 
-  /*!
-   * \brief Executes the command passed to the function.
-   *
-   * \param command The command to be executed.
-   *
-   * \returns If the execution was successful.
-   */
-  bool executeCommand(const CommandPtr& command);
+      /*!
+       * \brief Constructor of the cola2 session.
+       *
+       * \param async_tcp_client Pointer to an instance of a TCP-client. Will be used to establish a
+       * connection to the sensor.
+       */
+      explicit Cola2Session(const std::shared_ptr<communication::AsyncTCPClient> &async_tcp_client);
 
-  /*!
-   * \brief Returns the current session ID.
-   *
-   * \returns The current session ID.
-   */
-  uint32_t getSessionID() const;
+      /*!
+       * \brief Triggers the disconnection of the tcp socket.
+       */
+      void doDisconnect();
 
-  /*!
-   * \brief Sets the current session ID.
-   *
-   * \param session_id The new session ID.
-   */
-  void setSessionID(const uint32_t& session_id);
+      /*!
+       * \brief Executes the command passed to the function.
+       *
+       * \param command The command to be executed.
+       *
+       * \returns If the execution was successful.
+       */
+      bool executeCommand(const CommandPtr &command);
 
-  /*!
-   * \brief Returns the next request ID. The request ID is used to match the return packages of the
-   * sensor to the right command.
-   *
-   * \returns A new request ID.
-   */
-  uint16_t getNextRequestID();
+      /*!
+       * \brief Returns the current session ID.
+       *
+       * \returns The current session ID.
+       */
+      uint32_t getSessionID() const;
 
+      /*!
+       * \brief Sets the current session ID.
+       *
+       * \param session_id The new session ID.
+       */
+      void setSessionID(const uint32_t &session_id);
 
-  /*!
-   * \brief Closes a session with the sensor. Executes the close session command.
-   *
-   * \returns If closing the session was successful.
-   */
-  bool close();
+      /*!
+       * \brief Returns the next request ID. The request ID is used to match the return packages of the
+       * sensor to the right command.
+       *
+       * \returns A new request ID.
+       */
+      uint16_t getNextRequestID();
 
+      /*!
+       * \brief Closes a session with the sensor. Executes the close session command.
+       *
+       * \returns If closing the session was successful.
+       */
+      bool close();
 
-  /*!
-   * \brief Opens a session with the sensor. Executes the create session command.
-   *
-   * \returns If opening a session was successful.
-   */
-  bool open();
+      /*!
+       * \brief Opens a session with the sensor. Executes the create session command.
+       *
+       * \returns If opening a session was successful.
+       */
+      bool open();
 
+    private:
+      std::shared_ptr<sick::communication::AsyncTCPClient> m_async_tcp_client_ptr;
+      std::shared_ptr<sick::data_processing::ParseTCPPacket> m_parser_ptr;
+      std::shared_ptr<sick::data_processing::TCPPacketMerger> m_packet_merger_ptr;
+      std::shared_ptr<sick::data_processing::ParseTCPPacket> m_tcp_parser_ptr;
 
-private:
-  std::shared_ptr<sick::communication::AsyncTCPClient> m_async_tcp_client_ptr;
-  std::shared_ptr<sick::data_processing::ParseTCPPacket> m_parser_ptr;
-  std::shared_ptr<sick::data_processing::TCPPacketMerger> m_packet_merger_ptr;
-  std::shared_ptr<sick::data_processing::ParseTCPPacket> m_tcp_parser_ptr;
+      std::map<uint16_t, CommandPtr> m_pending_commands_map;
 
-  std::map<uint16_t, CommandPtr> m_pending_commands_map;
+      boost::mutex m_command_map_mutex;
+      boost::mutex m_execution_mutex;
 
-  boost::mutex m_execution_mutex;
+      uint32_t m_session_id;
+      uint16_t m_last_request_id;
 
-  uint32_t m_session_id;
-  uint16_t m_last_request_id;
+      void processPacket(const sick::datastructure::PacketBuffer &packet);
 
-  void processPacket(const sick::datastructure::PacketBuffer& packet);
+      bool addCommand(const uint16_t &request_id, const CommandPtr &command);
+      bool findCommand(const uint16_t &request_id, CommandPtr &command);
+      bool removeCommand(const uint16_t &request_id);
 
-  bool addCommand(const uint16_t& request_id, const CommandPtr& command);
-  bool findCommand(const uint16_t& request_id, CommandPtr& command);
-  bool removeCommand(const uint16_t& request_id);
+      bool
+      startProcessingAndRemovePendingCommandAfterwards(const sick::datastructure::PacketBuffer &packet);
+      bool addPacketToMerger(const sick::datastructure::PacketBuffer &packet);
+      bool checkIfPacketIsCompleteAndOtherwiseListenForMorePackets();
+      bool sendTelegramAndListenForAnswer(const CommandPtr &command);
+    };
 
-  bool
-  startProcessingAndRemovePendingCommandAfterwards(const sick::datastructure::PacketBuffer& packet);
-  bool addPacketToMerger(const sick::datastructure::PacketBuffer& packet);
-  bool checkIfPacketIsCompleteAndOtherwiseListenForMorePackets();
-  bool sendTelegramAndListenForAnswer(const CommandPtr& command);
-};
-
-
-} // namespace cola2
+  } // namespace cola2
 } // namespace sick
 
 #endif // SICK_SAFETYSCANNERS_COLA2_COLA2SESSION_H
